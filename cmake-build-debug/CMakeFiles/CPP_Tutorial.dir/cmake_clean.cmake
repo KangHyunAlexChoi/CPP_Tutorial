18c4754b7c77aadf0cd884ed217dc669fd28aeb5
Alex Choi <besticemg@gmail.com>
2020-08-29 09:59:29 +0900

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/CPP_Tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_Tutorial.dir/main.cpp.obj"
+  "CPP_Tutorial.exe"
+  "CPP_Tutorial.exe.manifest"
+  "CPP_Tutorial.pdb"
+  "libCPP_Tutorial.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_Tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
